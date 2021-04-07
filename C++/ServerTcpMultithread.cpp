5f0cb4ee99d6a50c0ae713f4fb55f5898415a67c
mfresi <55395016+mfresi@users.noreply.github.com>
2021-04-07 09:24:43 +0200

    implémentation du module bdd dans le serverTCPMultiClient

--- C++/ServerTcpMultithread.cpp
+++ C++/ServerTcpMultithread.cpp
@@ -1,3 +1,7 @@
+// ServerTCP avec multiClient qui gère la liaison entre le modbus et le site, écoute sur le port 9012.
+// Dev by Mattei FRESI.
+// command to compile file -> g++ ServerTcpMultithread.cpp -o output -L/usr/include/mariadb/mysql -lmariadbclient && ./output
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -7,6 +11,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <mysql/mysql.h>
 
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
@@ -15,8 +20,6 @@
 typedef int SOCKET;
 typedef struct sockaddr_in SOCKADDR_IN;
 typedef struct sockaddr SOCKADDR;
-
- //Mise en place d'un serveur TCP qui ecoute sur le port 9013
 
 int main()
 {
@@ -27,7 +30,7 @@ int main()
     int erreur = 0;
 #endif
 
-     //Création des deux sockets
+    // Initialisation des sockets et variables.
 
     SOCKET sock;
     SOCKADDR_IN sin;
@@ -40,7 +43,7 @@ int main()
     char bufferNiv2[512] = "haut";
     char bufferNiv3[512] = "haut";
     char bufferStopServer[512] = "Serveur arrêté";
-    char bufferClient[50] = { 0 };
+    char bufferClient[50] = {0};
     socklen_t recsize = sizeof(csin);
     int sockBind_err;
     int sockSend_err;
@@ -49,6 +52,16 @@ int main()
     int opt = TRUE;
     bool etat = false;
 
+    // Création de l'objet mysql.
+    MYSQL mysql;
+    char response;
+    int result;
+    const char * host = "mysql-projet-serre-eau.alwaysdata.net";
+    const char * login = "231030_mattei";
+    const char * password = "37pgmh55";
+    const char * bdd = "projet-serre-eau_bddeau";
+    const char * request = "INSERT INTO `consommation`(`eau_pluie`, `eau_courante`, `electrique`) VALUES (12, 45, 23);";
+
     if (!erreur)
     {
         // Création du socket
@@ -56,6 +69,7 @@ int main()
         // gestion d'erreur du socket
         if (sock != INVALID_SOCKET)
         {
+            // Méthode setsockopt qui permet la connexion multiClient.
             if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
             {
                 perror("setsockopt");
@@ -73,6 +87,16 @@ int main()
             if (sockBind_err != SOCKET_ERROR)
             {
                 printf("Patientez pendant que le client se connecte sur le port %d... \n", PORT);
+                // Connexion à la bade de donnée.
+                mysql_init(&mysql);
+                if (mysql_real_connect(&mysql, host, login, password, bdd, 0, NULL, 0))
+                {
+                    printf("Connexion à la BBD OK !\n");
+                }
+                else
+                {
+                    printf("Une erreur s'est produite lors de la connexion à la BDD!\n");
+                }
                 // Le serveur écoute se met en écoute et on accepte 6 connexions max.
                 listen(sock, 6);
                 // On reçoit le message envoyé par la source
@@ -81,7 +105,6 @@ int main()
                     // Le serveur accepte la communication avec la machine source
                     csock = accept(sock, (SOCKADDR *)&csin, &recsize);
                     printf("Un client se connecte avec la socket %d de %s sur le port %d \n", csock, inet_ntoa(csin.sin_addr), htons(csin.sin_port));
-
                     sockRecv_err = recv(csock, &bufferClient, sizeof(bufferClient), 0);
                     // Gestion d'erreur du nouveau socket
                     if (sockRecv_err == -1)
@@ -99,6 +122,8 @@ int main()
                         etat = true;
                         if (sockSend_err != SOCKET_ERROR)
                         {
+                            mysql_close(&mysql);
+                            printf("Connexion à la base fermée\n");
                             printf("Chaine envoyée  %s \n", bufferStopServer);
                         }
                         else
@@ -110,10 +135,23 @@ int main()
                     }
                     else if (bufferClient[0] == 't' && bufferClient[1] == 'e' && bufferClient[2] == 'm' && bufferClient[3] == 'p')
                     {
-                        //Envoyer la trame au server modbus pour recuperer la temperature
+                        //Envoyer la trame au server modbus pour recuperer la temperature.
+                        // Prend en paramètre un socket de type SOCKET, un buffer de type char, une taille de type int et un flag.
+                        // Return SOCKET_ERROR si problème rencontré.
                         sockSend_err = send(csock, bufferTemp, strlen(bufferTemp), 0);
                         if (sockSend_err != SOCKET_ERROR)
                         {
+                            result = mysql_real_query(&mysql, request, strlen(request));
+
+                            if (result == 0)
+                            {
+                                printf("Insertion en base OK !\n");
+                            }
+                            else
+                            {
+                                printf("Insertion en base PAS OK !\n");
+                            }
+
                             printf("Chaine envoyée  %s \n", bufferTemp);
                         }
                         else
@@ -125,7 +163,9 @@ int main()
                     }
                     else if (bufferClient[0] == 'n' && bufferClient[1] == 'i' && bufferClient[2] == 'v' && bufferClient[3] == '1')
                     {
-                        // Envoyer la trame au server modbus pour le niveau d'eau
+                        // Envoyer la trame au server modbus pour le niveau d'eau 1.
+                        // Prend en paramètre un socket de type SOCKET, un buffer de type char, une taille de type int et un flag.
+                        // Return SOCKET_ERROR si problème rencontré.
                         sockSend_err = send(csock, bufferNiv1, strlen(bufferNiv1), 0);
                         if (sockSend_err != SOCKET_ERROR)
                         {
@@ -140,7 +180,9 @@ int main()
                     }
                     else if (bufferClient[0] == 'n' && bufferClient[1] == 'i' && bufferClient[2] == 'v' && bufferClient[3] == '2')
                     {
-                        // Envoyer la trame au server modbus pour le niveau d'eau
+                        // Envoyer la trame au server modbus pour le niveau d'eau 2.
+                        // Prend en paramètre un socket de type SOCKET, un buffer de type char, une taille de type int et un flag.
+                        // Return SOCKET_ERROR si problème rencontré.
                         sockSend_err = send(csock, bufferNiv2, strlen(bufferNiv2), 0);
                         if (sockSend_err != SOCKET_ERROR)
                         {
@@ -155,7 +197,9 @@ int main()
                     }
                     else if (bufferClient[0] == 'n' && bufferClient[1] == 'i' && bufferClient[2] == 'v' && bufferClient[3] == '3')
                     {
-                        // Envoyer la trame au server modbus pour le niveau d'eau
+                        // Envoyer la trame au server modbus pour le niveau d'eau 3.
+                        // Prend en paramètre un socket de type SOCKET, un buffer de type char, une taille de type int et un flag.
+                        // Return SOCKET_ERROR si problème rencontré.
                         sockSend_err = send(csock, bufferNiv3, strlen(bufferNiv3), 0);
                         if (sockSend_err != SOCKET_ERROR)
                         {
@@ -170,6 +214,8 @@ int main()
                     }
                     else
                     {
+                        // Prend en paramètre un socket de type SOCKET, un buffer de type char, une taille de type int et un flag.
+                        // Return SOCKET_ERROR si problème rencontré.
                         sockSend_err = send(csock, bufferInvalid, strlen(bufferInvalid), 0);
                         if (sockSend_err != SOCKET_ERROR)
                         {
@@ -188,7 +234,7 @@ int main()
             {
                 printf("Pas reussi à bind le server \n");
             }
-
+            // Fermeture des sockets.
             printf("Fermeture de la socket... \n");
             close(sock);
             close(csock);
