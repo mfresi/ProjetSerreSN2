68517bc2dfb47d676d273a91184240064e65b1ae
Pilexlol <pprieux@la-providence.net>
2021-04-07 11:27:30 +0200

    ActiverActionneurs module test

--- C++/ActiverActionneurs/ConsoleApplication2/ActiverActionneurs.cpp
+++ C++/ActiverActionneurs/ConsoleApplication2/ActiverActionneurs.cpp
@@ -1,18 +1,26 @@
 // ConsoleApplication2.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
-
-
 #include <iostream>
 #include <Windows.h>
 #include <iostream>
 #include <cstdlib>
+#include <time.h> 
 	int main()
 	{
-		int temp = 10;
+		srand(time(NULL));
+		int temp;
+		temp = rand() % 10;
 		//cuve du bas
 		bool WaterLevel1 = 1;
 		//cuve du haut niveau haut
 		bool WaterLevel3 = 0;
+
+		std::cout << temp;
+		std::cout << " Degres" << std::endl;
+		std::cout << "Niveau de la cuve du bas : ";
+		std::cout << WaterLevel1 << std::endl;
+		std::cout << "Niveau de la cuve du haut : ";
+		std::cout << WaterLevel3 << std::endl;
 
 		//On active la pompe en fonction de la temperature, du niveau d'eau des cuves
 		if (temp < 2)
@@ -26,6 +34,7 @@
 		else if (WaterLevel3 == 0)
 		{    //On verifie si la cuve de pluie(haut) est déja remplie ou non(0 = Vide, 1 = remplie)
 			std::cout << "Pas assez d'eau dans la cuve de pluie (haut) \n";
+			std::cout << "On verifie la pompe \n";
 
 			if (temp <= 2)
 			{ //On verifie la temperature pour eviter que l'eau soit gelé (temp > 1 au minimum)
@@ -50,10 +59,10 @@
 					//On l'arrete au moment ou le Waterlvl3 arrive a 1
 					std::cout << "on laisse la pompe active \n";
 					Sleep(2000);
-					WaterLevel3 = 1;
-					std::cout << "on eteinds la pompe \n";
+					WaterLevel3 = rand() % 2;
 				}
-
+				std::cout << "Le niveau d'eau max a ete atteinds \n";
+				std::cout << "on eteinds la pompe \n";
 			}
 		}
 		if (temp >= 2 && WaterLevel1 == 1 && WaterLevel3 == 1)
